a5ceca581394093f5f16ee1aab05bca6bf2e6587
Anu Amallraja <anu9109@gmail.com>
2020-03-21 14:51:53 -0500

    the office plot split by season

--- 2020-03-17_theoffice.R
+++ 2020-03-17_theoffice.R
@@ -8,7 +8,7 @@ library(schrute)
 office_ratings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-17/office_ratings.csv')
 
 
-## Reproducing a graph from @drob's analysis
+## Based on a graph from @drob's analysis
 
 office_ratings  <- office_ratings %>%
   mutate(id = paste(season, episode, sep = "_")) %>% ### using id as ggplot x axis works for everything but geom_smooth() as it expects as numeric/computable input(?), so had to use row_number instead; other geoms work fine with id
@@ -35,15 +35,29 @@ popularity_plot <- office_ratings %>%
 
 ggsave(filename = "2020-03-17_theoffice_popularity_plot.png", plot = popularity_plot, device = "png") 
 
+#### split by season
+popularity_season_plot <- office_ratings %>% 
+  ggplot(aes(x = episode_number, y = imdb_rating)) + 
+  geom_point(aes(color = factor(season), size = total_votes)) +
+  geom_smooth() + 
+  facet_wrap(~season, scales = "free_x") + 
+  scale_color_viridis_d() +
+  theme_light() + 
+  theme(
+    axis.text.x = element_blank(),
+    axis.title.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = "none"
+  ) + 
+  labs(title = "Popularity of episode of the sitcom, The Office, over 9 seasons", 
+       subtitle = "Color indicates season, size indicates number of votes on IMDB", 
+       y = "IMDB Rating", 
+       caption = "Made for the #TidyTuesday project | @anu9109") 
 
-## Reproducing the original plots from the pudding's project at https://pudding.cool/2017/08/the-office/
-office_lines <- schrute::theoffice
+ggsave(filename = "2020-03-17_theoffice_popularity_season_plot.png", plot = popularity_season_plot, device = "png") 
 
-# Lines spoken during the entire series by characters who appeared in at least six episodes
 
-office_lines %>%
-  mutate(id = paste(season, episode, sep = "_")) %>% ## unique id for each episode
- # group_by(id) %>%
-  count(character, sort = T)
-  
+
+
+
 
